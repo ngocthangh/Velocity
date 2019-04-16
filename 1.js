92d6d25a1b141f5d8fb54954bd27547a81ff84f4
unknown <ngocthanguit@gmail.com>
2019-04-16 21:14:19 +0700

    finish theme with bootstrap 4

---
+++ 1.js
@@ -0,0 +1,20 @@
+﻿ $(function(){
+ 	$(window).scroll(function(){
+ 		console.log($('body').scrollTop());
+ 		if($(window).scrollTop() > 20) {
+			$('.navbar').addClass('down');
+ 		} else {
+ 			$('.navbar').removeClass('down');
+ 		}
+ 		if($(window).scrollTop() > 300) {
+			$('.nutlen').addClass('show');
+ 		} else {
+ 			$('.nutlen').removeClass('show');
+ 		}
+ 	})
+ 	$('.nutlen').click(function(){
+ 		$('html, body').animate({scrollTop:0}, 'slow');
+ 		return false;
+ 	})
+})  
+ 
